0372b968c74c1adae30687cb1e30a27c51388a9b
mglaser <glaser@hallowelt.biz>
2012-06-03 15:07:31 +0200

    initial commit of package This is the initial commit of the WPI package files and the additional folder that is added to mediawiki root directory.
    Change-Id: I944526a4835bb97131ed32a6db9f4215b613074f

---
+++ WpiSettings.php
@@ -0,0 +1,25 @@
+<?php
+
+$wgWPIOptionStore = array(
+		//Screen1
+		'lang' => 'PlaceholderForLanguage',
+		//Screen2
+		'dbserver' => 'PlaceholderForDbServer',
+		'dbname' => 'PlaceholderForDbName',
+		'dbprefix' => 'PlaceholderForDbPrefix',
+		'dbuser' => 'PlaceholderForDbUsername',
+		'dbpass' => 'PlaceholderForDbPassword',
+		'installdbuser' => 'PlaceholderForDbAdminUsername',
+		'installdbpass' => 'PlaceholderForDbAdminPassword',
+		//Screen3
+		'sitename' => 'PlaceholderForSitename',
+		'adminname' => 'PlaceholderForAdminname',
+		'pass' => 'PlaceholderForAdminpass',
+		
+		'scriptpath' => 'PlaceholderForScriptPath',
+	
+		'usewindowsazure' => 'PlaceholderForUseAzure',
+		'azurehost' => 'PlaceholderForAzureHost',
+		'azureaccount' => 'PlaceholderForAzureAccount',
+		'azurekey' => 'PlaceholderForAzureKey'
+	);
