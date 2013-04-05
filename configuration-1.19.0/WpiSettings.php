a3e839295d9af430dd8d124d17a30f7a0eee3cfa
mglaser <glaser@hallowelt.biz>
2013-04-05 12:16:37 +0200

    Moved configuration files to version specific subfolder
    Change-Id: Ia2225955e364761ea00679466b3eacc4b0dfa733

---
+++ configuration-1.19.0/WpiSettings.php
@@ -0,0 +1,26 @@
+<?php
+
+$wgWPIOptionStore = array(
+		//Screen1
+		'lang' => 'PlaceholderForLanguage',
+		//Screen2
+		'dbserver' => 'PlaceholderForDbServer',
+		'dbname' => 'PlaceholderForDbName',
+		'dbprefix' => 'PlaceholderForDbPrefix',
+		'dbuser' => 'PlaceholderForDbUsername',
+		'dbpass' => 'PlaceholderForDbPassword',
+		'installdbuser' => 'PlaceholderForDbAdminUsername',
+		'installdbpass' => 'PlaceholderForDbAdminPassword',
+		//Screen3
+		'sitename' => 'PlaceholderForSitename',
+		'adminname' => 'PlaceholderForAdminname',
+		'pass' => 'PlaceholderForAdminpass',
+		
+		'scriptpath' => 'PlaceholderForScriptPath',
+	    'enablefileupload' => 'PlaceholderForFileUpload',
+		'usewindowsazure' => 'PlaceholderForUseAzure',
+		'azurehost' => 'PlaceholderForAzureHost',
+		'azureaccount' => 'PlaceholderForAzureAccount',
+		'azurekey' => 'PlaceholderForAzureKey',
+        'wikiId' => 'PlaceholderForWikiId'
+	);
---
+++ configuration-1.19.0/mediawiki/mw-wpi/index.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * New version of MediaWiki web-based config/installation
+ *
+ * @file
+ */
+
+define( 'MW_CONFIG_CALLBACK', 'Installer::overrideConfig' );
+define( 'MEDIAWIKI_INSTALL', true );
+
+chdir( dirname( dirname( __FILE__ ) ) );
+if ( isset( $_SERVER['MW_COMPILED'] ) ) {
+	require ( 'phase3/includes/WebStart.php' );
+} else {
+	require( dirname( dirname( __FILE__ ) ) . '/includes/WebStart.php' );
+}
+
+require( dirname( __FILE__ ) . '/WpiSettings.php' );
+
+wfInstallerMain();
+
+function wfInstallerMain() {
+//	global $wgRequest, $wgLang, $wgMetaNamespace, $wgCanonicalNamespaceNames;
+	global $IP, $wgWPIOptionStore, $wgLanguageCode, $wgEnableUploads;
+
+	$siteName = $wgWPIOptionStore['sitename'];
+	$adminName = $wgWPIOptionStore['adminname'];
+	$wgLanguageCode = $wgWPIOptionStore['lang'];
+	$wgEnableUploads = (strtolower($wgWPIOptionStore['enablefileupload']) == 'true');
+	
+	$wgWPIOptionStore['scriptpath'] = getScriptPath();
+	
+	ob_start();
+	echo "<pre>";
+	
+	$installer = new CliInstaller( $siteName, $adminName, $wgWPIOptionStore );
+	
+	$extensions = array();
+	if ( strtolower($wgWPIOptionStore['usewindowsazure']) == "true" ) {
+		$extensions[] = 'WindowsAzureStorage';
+		$extensions[] = 'WindowsAzureSDK';
+	}
+	
+	$installer->setVar( '_Extensions', $extensions );
+
+	$status = $installer->doEnvironmentChecks();
+	if( $status->isGood() ) {
+		$installer->showMessage( 'config-env-good' );
+		$installer->execute();
+		$installer->writeConfigurationFile( $IP );
+		// Modify LocalSettings for WindowsAzure
+		if ( strtolower($wgWPIOptionStore['usewindowsazure']) == "true" ) {
+			$azureSettings = "
+\$wgFileBackends[] = array(
+		'name'        => 'azure-backend',
+		'class'       => 'WindowsAzureFileBackend',
+		'lockManager' => 'nullLockManager',
+		'azureHost'      => '{$wgWPIOptionStore['azurehost']}',
+		'azureAccount'   => '{$wgWPIOptionStore['azureaccount']}',
+		'azureKey'       => '{$wgWPIOptionStore['azurekey']}',
+
+		//IMPORTANT: Mind the container naming conventions! http://msdn.microsoft.com/en-us/library/dd135715.aspx
+		'containerPaths' => array(
+				'media-public'  => 'media-public',
+				'media-thumb'   => 'media-thumb',
+				'media-deleted' => 'media-deleted',
+				'media-temp'    => 'media-temp',
+
+		)
+);
+
+\$wgLocalFileRepo = array (
+	'class'             => 'LocalRepo',
+	'name'              => 'local',
+	'scriptDirUrl'      => '/php/mediawiki-filebackend-azure',
+	'scriptExtension'   => '.php',
+	'url'               => \$wgScriptPath.'/img_auth.php', // It is important to set this to img_auth. Basically, there is no alternative.
+	'hashLevels'        => 2,
+	'thumbScriptUrl'    => false,
+	'transformVia404'   => false,
+	'deletedHashLevels' => 3,
+	'backend'           => 'azure-backend',
+	'zones' => 
+			array (
+					'public' => 
+							array (
+								'container' => 'local-public',
+								'directory' => '',
+							),
+					'thumb' => 
+							array(
+								'container' => 'local-public',
+								'directory' => 'thumb',
+							),
+					'deleted' => 
+							array (
+								'container' => 'local-public',
+								'directory' => 'deleted',
+							),
+					'temp' => 
+							array(
+								'container' => 'local-public',
+								'directory' => 'temp',
+							)
+		)
+);
+
+\$wgImgAuthPublicTest = false;
+";
+			file_put_contents( $IP.'/LocalSettings.php', $azureSettings, FILE_APPEND );
+		}
+	}
+	echo "</pre>";
+	
+	ob_end_clean();
+	
+	header('Location: ../');
+}
+
+/**
+ * Get the script path
+ * 
+ * @return String $wpiScriptPath The web platform installer script path
+ */
+
+function getScriptPath() {
+    //setting the default value as empty
+    $wpiScriptPath = '';
+    $path = false;
+	if ( !empty( $_SERVER['PHP_SELF'] ) ) {
+		$path = $_SERVER['PHP_SELF'];
+	} elseif ( !empty( $_SERVER['SCRIPT_NAME'] ) ) {
+		$path = $_SERVER['SCRIPT_NAME'];
+	}
+	if ($path !== false) {
+		$wpiScriptPath = preg_replace( '{^(.*)/(mw-)?wpi.*$}', '$1', $path );
+	}
+	return $wpiScriptPath;
+}
