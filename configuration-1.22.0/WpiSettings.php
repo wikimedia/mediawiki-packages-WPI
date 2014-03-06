6c9e8b8542150b1f6e9ce4bfdc1b918ddc5867d0
mglaser <glaser@hallowelt.biz>
2014-03-06 11:43:01 +0100

    Updated to MW1.22.0
    Changelog
    * Updated the core to 1.22.0.
    * Updated the WindowsAzureSDK and WindowsAzureStorage extensions to the
    * latest version.
    * Fixed a bug with parameter type in AzureFileBackendList class
      constructor in WindowsAzureStorage extension.
    * Changed web deploy parameter names to user friendly names.
    * Added few validations for web deploy parameters.
    * Removed the azure host parameter since it is not required in the latest
      extension.
    * Changed the values of "Enable MediaWiki file upload" and "Use Windows
      Azure storage as file backend" parameters to Yes/No instead of
      True/False.
    * The "mw-config/index.php"  file will be replaced with the default file
      (WebInstaller) after installation. Since command line is not available
      in Windows Azure websites, this will help the users to upgrade MediaWiki
      using the upgrade key through browser.
    * The WpiSettings.php file will be deleted once the installation is
      completed.
    
    This is a package release, thanks to Pravin Deshmukh
    
    Change-Id: Ia75ac6170bb85d17fe79495c02c183a908adeb5b

---
+++ configuration-1.22.0/WpiSettings.php
@@ -0,0 +1,25 @@
+<?php
+
+$wgWPIOptionStore = array(
+		//Screen1
+		'lang' => 'PlaceholderForLanguage',
+		//Screen2
+		'dbserver' => 'PlaceholderForDbServer',
+		'dbname' => 'PlaceholderForDbName',
+		'dbprefix' => 'PlaceholderForDbPrefix',
+		'dbuser' => 'PlaceholderForDbUsername',
+		'dbpass' => 'PlaceholderForDbPassword',
+		'installdbuser' => 'PlaceholderForDbAdminUsername',
+		'installdbpass' => 'PlaceholderForDbAdminPassword',
+		//Screen3
+		'sitename' => 'PlaceholderForSitename',
+		'adminname' => 'PlaceholderForAdminname',
+		'pass' => 'PlaceholderForAdminpass',
+		
+		'scriptpath' => 'PlaceholderForScriptPath',
+	    'enablefileupload' => 'PlaceholderForFileUpload',
+		'usewindowsazure' => 'PlaceholderForUseAzure',
+		'azureaccount' => 'PlaceholderForAzureAccount',
+		'azurekey' => 'PlaceholderForAzureKey',
+        'wikiId' => 'PlaceholderForWikiId'
+	);
